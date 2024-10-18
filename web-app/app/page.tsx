f991de810d4b16ead12bb43cbede767510cc67cd
robocoder-repo <robocoder-repo@blackboxai.tech>
2024-10-18 19:07:46 -0400

    Add web-app/app/page.tsx

---
+++ web-app/app/page.tsx
@@ -0,0 +1,10 @@
+
+import ChatInterface from '../components/chat-interface'
+
+export default function Home() {
+  return (
+    <div className="container mx-auto p-4">
+      <ChatInterface />
+    </div>
+  )
+}
